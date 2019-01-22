e0950e53ae275816d40584ddef5b805aaecd4372
rchow1944 <rchow2@buffalo.edu>
2019-01-22 15:57:11 -0500

    Working on display user characters cli

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -8,5 +8,8 @@ def get_user_name
 end
 
 def display_user_characters(user)
-  puts "Hello, #{user}"
+  found_user = User.find_or_create_by(name: user)
+  found_user.characters.each_with_index { |character, i|
+    puts "#{i+1}. #{character.name}"
+  }
 end
