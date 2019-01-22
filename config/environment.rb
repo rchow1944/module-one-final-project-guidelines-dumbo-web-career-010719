b6fc60d48454280d5766f01fdc8aaf41302aa5f2
rchow1944 <rchow2@buffalo.edu>
2019-01-22 16:29:04 -0500

    fixed merge issues

--- config/environment.rb
+++ config/environment.rb
@@ -1,20 +1,26 @@
 require 'bundler/setup'
 Bundler.require
 
-require 'rake'
-require 'active_record'
-require 'yaml/store'
-require 'ostruct'
-require 'date'
-
-DBNAME = "champions"
-
-Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
-Dir[File.join(File.dirname(__FILE__), "../lib/support", "*.rb")].each {|f| require f}
-
-DBRegistry[ENV["ACTIVE_RECORD_ENV"]].connect!
-DB = ActiveRecord::Base.connection
-
-if ENV["ACTIVE_RECORD_ENV"] == "test"
-  ActiveRecord::Migration.verbose = false
-end
+# <<<<<<< HEAD
+ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
+require_all 'lib'
+require_all 'app'
+# =======
+# require 'rake'
+# require 'active_record'
+# require 'yaml/store'
+# require 'ostruct'
+# require 'date'
+#
+# DBNAME = "champions"
+#
+# Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
+# Dir[File.join(File.dirname(__FILE__), "../lib/support", "*.rb")].each {|f| require f}
+#
+# DBRegistry[ENV["ACTIVE_RECORD_ENV"]].connect!
+# DB = ActiveRecord::Base.connection
+#
+# if ENV["ACTIVE_RECORD_ENV"] == "test"
+#   ActiveRecord::Migration.verbose = false
+# end
+# >>>>>>> effab1b74d3856a18c67e31976cabb25a606bc1b
--- db/migrate/20190122185057_create_guilds.rb
+++
@@ -1,8 +0,0 @@
-class CreateGuilds < ActiveRecord::Migration[5.0]
-  def change
-    create_table :guilds do |t|
-      t.string :name
-      t.string :owner
-    end
-  end
-end
