99a97df68d6b40fa6adb5434ea0534a492c34181
rchow1944 <rchow2@buffalo.edu>
2019-01-22 12:14:20 -0500

    defined model classes for active record

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,6 @@
 require_relative '../config/environment'
-require_relative '../lib/command_line_interface.rb'
+# require_relative '../lib/command_line_interface.rb'
+# require_relative '../app/models/guild.rb'
 
 welcome
 user = get_user_name
